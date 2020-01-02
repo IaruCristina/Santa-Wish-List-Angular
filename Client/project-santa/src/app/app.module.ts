ef445ac80d6fb91d1b885dbbbbbf3a44da5159c0
Pusky <puscasualexandru10@gmail.com>
2020-01-02 19:19:38 +0200

    Added Material UI component.

--- Client/project-santa/src/app/app.module.ts
+++ Client/project-santa/src/app/app.module.ts
@@ -5,14 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+
+const appRoutes: Routes = [
+  { path: 'startPage', component: MainPageComponent }
+];
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
