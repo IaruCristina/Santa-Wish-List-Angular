781d1a22d9da10cae0b7d53a083e1037f641b098
Cristina <iaru.cristina99@yahoo.com>
2020-01-03 22:02:10 +0200

    Populated data for Santa list

--- Client/project-santa/src/app/santa/santa-list.ts
+++ Client/project-santa/src/app/santa/santa-list.ts
@@ -2,10 +2,13 @@ import { Santa } from './santa'
 
 /* SANTA_LIST - instead of excel file */
 export const SANTA_LIST: Santa[] = [
-    {id: 1, name: 'Pusky', country: 'Romania', description: 'I am santa', photo: 'https://i.imgur.com/YtT1tKt.jpg', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
-    {id: 2, name: 'Cris', country: 'Germany', description: 'I am santa', photo: 'None', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
-    {id: 3, name: 'Santa', country: 'Finland', description: 'I am santa', photo: 'None', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
-    {id: 4, name: 'Some other guy', country: 'Chile', description: 'I am santa', photo: 'None', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
-    {id: 5, name: 'Mr. Santa', country: 'Japan', description: 'I am santa', photo: 'None', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
-    {id: 6, name: 'Mr. Santa', country: 'Japan', description: 'I am santa', photo: 'None', song: 'None', wishlist:['Sarmale', 'Mamaliga']}
+    {id: 1, name: 'Father Christmas', country: 'United Kingdom', description: 'the traditional British Father Christmas wears a hooded green cloak, a wreath of holly or ivy, and carries a staff.', photo: 'https://cutt.ly/druePVT', song: 'None', wishlist:['candy','apples']},
+    {id: 2, name: 'Père Noël', country: 'France', description: 'He wears a long red cloak instead of a red suit, and children leave their shoes by the fireplace hoping that they will be filled with goodies after mass on Christmas Eve.', photo: 'https://cutt.ly/urueHqX', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
+    {id: 3, name: 'Ded Moroz', country: 'Russia', description: 'Traditionally, he would punish naughty children by kidnapping them, but this darker part of Ded Moroz.s personality has softened over the years.', photo: 'https://cutt.ly/1rueHGH', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
+    {id: 4, name: 'Tomte', country: 'Sweden', description: 'Traditionally a dwarf-like being with the appearance of a garden gnome, the tomte guards farmhouses from bad luck. ', photo: 'https://cutt.ly/krueKmy', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
+    {id: 5, name: 'Julenissen', country: 'Norway', description: 'Julenisse wears grey clothes and usually has a grey beard instead of a white one.', photo: 'https://cutt.ly/QrueZVz', song: 'None', wishlist:['Sarmale', 'Mamaliga']},
+    {id: 6, name: 'Mos Craciun', country: 'Romania', description: 'The REAL Santa', photo: 'https://cutt.ly/SrueCtF', song: 'None', wishlist:['nuia', 'carbuni']},
+    {id: 7, name: 'Sinterklaas', country: 'The Netherlands', description: 'Sinterklaas comes riding into town on a white horse, wearing a tall, red bishop.s hat and a jeweled staff. ', photo: 'https://cutt.ly/srurvtM', song: 'None', wishlist:['nuia', 'carbuni']},
+    {id: 8, name: 'Christkind', country: 'Austria', description: 'Christkind or "Christ Child" is the benevolent (usually female) gift-bringer who leaves presents for good children on Christmas Eve, much like her more famous counterpart, Santa Claus.', photo: 'https://cutt.ly/hrurb7X', song: 'None', wishlist:['nuia', 'carbuni']},
+    {id: 9, name: 'Los Reyes Magos', country: 'Spain', description: 'In Spain, good children are not only visited by one, but three jolly figures who disseminate presents on El Dia De Reyes, the day that the three wise men (or magi) finally reached baby Jesus on January 6.', photo: 'https://cutt.ly/HrurWWP', song: 'None', wishlist:['nuia', 'carbuni']}
 ];
